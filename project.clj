100bb000733919537d602047f6490a2fa7460b7f
Mike Fikes <mike@fikesfarm.com>
2018-05-11 06:33:01 -0400

    Update name in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljsrn/side-fx "0.1.0"
+(defproject side-fx "0.1.0"
   :description "re-frame effect handlers for React Native"
   :url "https://github.com/cljsrn/side-fx"
   :plugins [[lein-tools-deps "0.3.0-SNAPSHOT"]]
